27fbc1210a5463a650ac9b3a8ff5b87edc7e8a87
Francisco González <fxgonzalez5@utpl.edu.ec>
2024-02-23 12:20:18 -0500

    Modulos completados, incluyendo la visualización de vídeo

--- lib/presentation/services/firebase_auth_service.dart
+++ lib/presentation/services/firebase_auth_service.dart
@@ -30,8 +30,8 @@ class FirebaseAuthService {
         final userEntity = UserEntity(
           id: credential.user!.uid,
           email: email,
-          name: 'Francisco González',
-          photoUrl: 'https://avatars.githubusercontent.com/u/73602789?s=400&u=dd0f4dc89c961c77ad50d803cf4310e7b95a8152&v=4'
+          name: 'Nombre y Apellido', // Modificar para agregarle un nombre al usuario
+          photoUrl: '' // Modificar para agregarle una foto al usuario
         );
 
         Get.find<CloudDatabaseRepositoryImpl>().saveUser(userEntity);
